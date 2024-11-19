875d17b2aaf93eaea517fb9cead0d108e30708a0
stalkeuar <mai888sutalkuar@gmail.com>
2024-11-19 08:37:58 +0000

    commit

---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	char name[50];
+
+	std::cout << "Enter the name: " << std::endl;
+	std::cin >> name;
+	std::cout << "Hello " << name << "!" << std::endl;
+
+return 0;
+}
