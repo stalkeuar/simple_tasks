1e77467b38a66699924e9029e3a30ec4fa0277b8
stalkeuar <mai888sutalkuar@gmail.com>
2024-11-19 09:06:36 +0000

    commit

---
+++ task3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	int b;
+
+	std::cin >> a;
+	std::cin >> b;
+	std::cout << a + b << std::endl;
+	std::cout << a - b << std::endl;
+	std::cout << a * b << std::endl;
+
+return 0;
+}
